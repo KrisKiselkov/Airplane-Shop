289243b0147fe81e9288e67026ee1714f4a744e0
KrisKiselkov <cristian.kiselkov@gmail.com>
2024-03-19 13:24:13 +0200

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import { Nav } from './content/Nav';
 import { Home } from './content/Home';
+import { Planes } from './content/Planes';
 import './App.css';
 
 
 function App() {
-  
 
   return (
     <>
       <Nav />
 
-      <Home/>
+      <Home />
+
+      <Planes />
     </>
   );
 }
