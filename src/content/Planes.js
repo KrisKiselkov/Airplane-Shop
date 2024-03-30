9e97123f6e42d15be1c347f99a84f884bb8d83be
KrisKiselkov <cristian.kiselkov@gmail.com>
2024-03-30 17:19:08 +0200

    See All Functionality

--- src/content/Planes.js
+++ src/content/Planes.js
@@ -83,6 +83,86 @@ export const planesObject = [
         price: '$ 445,000,000',
         alt: 'A380',
         id: 380
+    },
+    {
+        image: plane,
+        fullType: 'Airbus 220-300',
+        type: 'A220-300',
+        description: ['Lorem ipsum aliqua magna sit amet ver dolor.Lorem ipsum aliqua magna sit amet ver dolor.' ,'4 years', '34m x 40m', '197 pax'],
+        brand: 'Airbus',
+        price: '$ 420,000,000',
+        alt: 'A220',
+        id: 220
+    },
+    {
+        image: plane,
+        fullType: 'Boeing 777-9',
+        type: 'B777-9',
+        brand: 'Boeing',
+        description: ['Lorem ipsum aliqua magna sit amet ver dolor.Lorem ipsum aliqua magna sit amet ver dolor.' ,'4 years', '34m x 40m', '197 pax'],
+        price: '$ 155,000,000',
+        alt: 'B777',
+        id: 777
+    },
+    {
+        image: plane,
+        fullType: 'Boeing 767-400',
+        type: 'B767-400',
+        brand: 'Boeing',
+        description: ['Lorem ipsum aliqua magna sit amet ver dolor.Lorem ipsum aliqua magna sit amet ver dolor.' ,'4 years', '34m x 40m', '197 pax'],
+        price: '$ 240,000,000',
+        alt: 'B767',
+        id: 767
+    },
+    {
+        image: plane,
+        fullType: 'Airbus A321-231',
+        type: 'A321-231',
+        brand: 'Airbus',
+        description: ['Lorem ipsum aliqua magna sit amet ver dolor.Lorem ipsum aliqua magna sit amet ver dolor.' ,'4 years', '34m x 40m', '197 pax'],
+        price: '$ 300,000,000',
+        alt: 'A321',
+        id: 321
+    },
+    {
+        image: plane,
+        fullType: 'Airbus A350-1000',
+        type: 'A350-1000',
+        brand: 'Airbus',
+        description: ['Lorem ipsum aliqua magna sit amet ver dolor.Lorem ipsum aliqua magna sit amet ver dolor.' ,'4 years', '34m x 40m', '197 pax'],
+        price: '$ 275,000,000',
+        alt: 'A350',
+        id: 350
+    },
+    {
+        image: plane,
+        fullType: 'Boeing 757-200',
+        type: 'B757-200',
+        description: ['Lorem ipsum aliqua magna sit amet ver dolor.Lorem ipsum aliqua magna sit amet ver dolor.' ,'4 years', '34m x 40m', '197 pax'],
+        brand: 'Boeing',
+        price: '$ 420,000,000',
+        alt: 'B757',
+        id: 757
+    },
+    {
+        image: require('./images/plane.jpg') ,
+        type: 'A400M',
+        fullType: 'Airbus A400M',
+        brand: 'Airbus',
+        description: ['Lorem ipsum aliqua magna sit amet ver dolor.Lorem ipsum aliqua magna sit amet ver dolor.' ,'4 years', '34m x 40m', '197 pax'],
+        price: '$ 445,000,000',
+        alt: 'A400M',
+        id: 400
+    },
+    {
+        image: plane,
+        fullType: 'Boeing C-17',
+        type: 'C-17',
+        description: ['Lorem ipsum aliqua magna sit amet ver dolor.Lorem ipsum aliqua magna sit amet ver dolor.' ,'4 years', '34m x 40m', '197 pax'],
+        brand: 'Boeing',
+        price: '$ 420,000,000',
+        alt: 'C17',
+        id: 17
     }
 ];
 
@@ -108,14 +188,30 @@ export function Planes() {
         }
     }
 
+    const planeOverflow = () => {
+        const h = document.getElementById('all-h3');
+        const col = document.getElementById('collection');
+
+        const onClick = () => {
+            if (col.style.height === '620px') {
+                col.style.height = 'auto';
+                h.innerHTML = 'See Less';
+            } else {
+                col.style.height = '620px';
+                h.innerHTML = 'See All';
+            }
+        }
+
+        h.addEventListener('click', onClick);
+    }
+
     return (
         <>
             <h1 id="main-h1">Plane Collection</h1>
             <section id='collection'>
                 {planeFunc()}
-
-                <h3 id='all-h3'>See All</h3>
             </section>
+            <h3 id='all-h3' onClick={planeOverflow}>See All</h3>
         </>
     )
 }
